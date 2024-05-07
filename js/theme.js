13254f14617dd0a31aad3a18bee97cb08a6e6836
Mohammad Reza Naseri <113291086+MohammadRezaNaseri@users.noreply.github.com>
2024-05-07 15:55:49 +0330

    first publish

---
+++ js/theme.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+	  items: 4,
+      rtl: true,
+      loop: true,
+      dots: true,
+      nav: true,
+      navText: ['<i class="fa fa-angle-right"></i>', '<i class="fa fa-angle-left"></i>'],
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+	  responsive:{
+		0:{
+			items:1
+		},
+		600:{
+			items:2
+		},
+		1000:{
+			items:4
+		}
+	  }
+  });
+  
+  var swiper = new Swiper(".mySwiper", {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".mySwiper2", {
+      loop: true,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiper,
+      },
+    });
+  
+});
